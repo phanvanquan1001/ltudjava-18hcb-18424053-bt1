e16b09de6eb6d3b6740a3f1145a2070971661da1
phanvanquan1001 <49383264+phanvanquan1001@users.noreply.github.com>
2019-07-31 16:55:31 +0700

    sssss

--- QLDIEM/src/qldiem/DSLOP.java
+++ QLDIEM/src/qldiem/DSLOP.java
@@ -3,19 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package QUANLYSINHVIEN.MAIN;
+package qldiem;
 
 /**
  *
  * @author minhq
  */
-public class ImportSV {
-    private int id;
-    private String code;
-    private String name;
- 
-    public Country() {
-        super();
-    }
+public class DSLOP {
     
 }
---
+++ QLDIEM/src/qldiem/QLDIEM.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package qldiem;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+/*Quan ly 1 mang hs gồm 
+HSCap 1(MaHS, TenHS, DiemToan, DiemVan)
+HSCap 2(MaHS, TenHS, DiemToan, DiemVan,DiemLy)
+HSCap 3(MaHS, TenHS, DiemToan, DiemVan,DiemTiengAnh)
+Nhap va xuat 1 mang HS.
+Xuat ra xep loai cua tung HS trong mang.*/
+/**
+ *
+ * @author minhq
+ */
+public class QLDIEM {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
