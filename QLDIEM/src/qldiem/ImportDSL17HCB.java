52875e973b841f0ac2845f2f28edf041eb245c59
phanvanquan1001 <49383264+phanvanquan1001@users.noreply.github.com>
2019-07-31 23:07:36 +0700

    sưdwd

---
+++ QLDIEM/src/qldiem/ImportDSL17HCB.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package qldiem;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.io.FileWriter;
+
+/**
+ *
+ * @author minhq
+ */
+public class ImportDSL17HCB {
+     /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ImportFileCSV lop = new ImportFileCSV();
+        lop.ImportFile("17HCB.CSV", "17HCBOUT.CSV");
+    }
+}
---
+++ QLDIEM/src/qldiem/ImportFileCSV.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package qldiem;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+/**
+ *
+ * @author minhq
+ */
+public class ImportFileCSV {
+    void ShowFile( String  namefile) {
+        try {
+     //Bước 1: Tạo đối tượng luồng và liên kết nguồn dữ liệu
+     
+            String currentDirectory = System.getProperty("user.dir")+"\\"+ namefile;
+            File f = new File(currentDirectory );
+            System.out.println(currentDirectory);
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(f), "UTF8"))) {
+                String line;
+                while ((line = br.readLine()) != null){
+                    System.out.println(line);
+                    //String[] output = line.split(",");
+                    //System.out.println(output[0]);
+                }
+            }
+         } 
+        catch (IOException ex) 
+        {
+        System.out.println("Loi đọc file: " + ex);
+        }
+    
+     }
+    
+    
+     void ImportFile( String importInput , String importOutput ) {
+        try {
+     //Bước 1: Tạo đối tượng luồng và liên kết nguồn dữ liệu
+     
+            String inputDirectory = System.getProperty("user.dir")+"\\"+ importInput;
+            String outputDirectory = System.getProperty("user.dir")+"\\"+ importOutput;
+            File input = new File(inputDirectory );
+            File output = new File(outputDirectory );
+            BufferedReader br;
+            try (FileWriter fw = new FileWriter(output, true)) {
+                br = new BufferedReader(new InputStreamReader(new FileInputStream(input), "UTF8"));
+                String line;
+                while ((line = br.readLine()) != null){
+                    System.out.println(line);
+                    fw.write("\n" + line);
+                    
+                    //String[] output = line.split(",");
+                    //System.out.println(output[0]);
+                }
+            }
+            br.close();
+         } 
+        catch (IOException ex) 
+        {
+        System.out.println("Loi ghi file: " + ex);
+        }
+    
+     }
+    
+}
