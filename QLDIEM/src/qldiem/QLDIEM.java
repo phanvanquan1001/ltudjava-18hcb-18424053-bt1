82ef4143a04d418f247bc7a3ce6758f25a5fea14
phanvanquan1001 <49383264+phanvanquan1001@users.noreply.github.com>
2019-08-02 14:52:01 +0700

    qqqqqqqqqqqqq

--- QLDIEM/src/qldiem/QLDIEM.java
+++ QLDIEM/src/qldiem/QLDIEM.java
@@ -5,7 +5,15 @@
  */
 package qldiem;
 
-import java.util.ArrayList;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.io.FileWriter;
 import java.util.Scanner;
 /*Quan ly 1 mang hs gồm 
 HSCap 1(MaHS, TenHS, DiemToan, DiemVan)
@@ -17,64 +25,73 @@ Xuat ra xep loai cua tung HS trong mang.*/
  *
  * @author minhq
  */
-public class QLDIEM extends ImportFile{
+public class QLDIEM{
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-         Scanner sc = new Scanner(System.in);
-        //lấy user nhạp từ bàn phím
-        System.out.print("Username: ");
-        String user=sc.nextLine();
-         //lấy pass nhạp từ bàn phím
-        System.out.print("Password: ");
-        String pass=sc.nextLine();
-        try{
-        if(user.equals("")||pass.equals("")){
-        System.out.println("Vui lòng nhập đủ thông tin.");}
-        if(user.equals("giaovu")&&pass.equals("giaovu")){
-            System.out.println("Đây là chức năng dành cho Giáo Vụ");
-             // TODO code application logic here
-             ArrayList<ImportFile> mang=new ArrayList();
-              while (true) {
-            System.out.println("Ban muon nhap gi 1. Thêm SV 2. ImportDSL17 3. ImportDSL18  4. ket thuc");
-            Scanner a = new Scanner(System.in);
-            int chon = a.nextInt();
-            ImportFile b = null;
-            if (chon == 1) {
-                b = new AddSV();
-            }
-            if (chon == 2) {
-                b = new ImportDSL17HCB();
-            }
-            if (chon == 3) {
-                b = new ImportDSL18HCB();
-            }
-            
-            if (chon == 4) {
-                break;
-            }
-            b.nhap();
-            mang.add(b);
-
-        }
-        for(int i=0;i<mang.size();i++)
+        
+        NewClass lop = new NewClass(); 
+        Scanner scanner = new Scanner(System.in);
+        int 17HCB = 0;
+        int 18HCB =0;
+        int giaovu =0 ;
+        String username ;
+        
+        while(true)
         {
-            mang.get(i).ShowFile( "17HCB.CSV");
-            mang.get(i).xeploaiSV();
+            System.out.println("Moi ban dang nhap\n");
+            System.out.println("Username: ");
+            username = scanner.next();
+            System.out.println("Password: ");
+            String password = scanner.next();        
+            17HCB =lop.login(username, password, "17HCB.csv");
+            18HCB = lop.login(username, password, "18HCB.csv");
+            //giaovu = lop.login(username, password, "giaovu.csv");
+            if (17HCB==1 || 18HCB==1 || giaovu== 1 ) break;
+            else {
+                System.out.println("Vui lòng kiểm tra lại username hoặc password");
+            }
         }
-          }
+        if(17HCB ==1 | 18HCB ==1){
+            while(true){
+                System.out.println("CHÀO MỪNG ĐẾN VỚI CỔNG LÀM VIỆC CỦA GIÁO VỤ\n");
+         
+                System.out.println("Chọn Chức Năng\n");
+                System.out.println("1: Xem Điểm Môn Java\n");
+                System.out.println("2: Xem Điểm Môn KCPM\n");
+                System.out.println("3: Xem Điểm Môn MMT\n");
+                System.out.println("4: Xem Điểm Môn TKGD\n");
+                
+                
+                int value = scanner.nextInt();
+                
+                switch (value) {
+                case 1:
+                lop.showinfor(username,"danhsachJava.csv");
+                //lop.importfile("importkhoa2017.csv", "khoa2017.csv"); 
+                break;
+                case 2:
+                lop.showinfor(username, "danhsachKCPM.csv");         
+                //lop.importfile("importkhoa2017.csv", "khoa2017.csv"); 
+                break;
+                case 3:
+                lop.showinfor(username, "danhsachMMT.csv");         
+                //lop.importfile("importkhoa2017.csv", "khoa2017.csv"); 
+                break;
+                case 4:
+                lop.showinfor(username, "danhsachTKGD.csv");         
+                //lop.importfile("importkhoa2017.csv", "khoa2017.csv"); 
+                break;
+                }
         
         
-        else{
-            System.out.println("Sai User hoặc Pass");
-        }
-        }
-        catch(Exception e){
-            System.out.println("Kết nối thất bại");
-          
+        
+        
+        
+         
         }
     }
     
